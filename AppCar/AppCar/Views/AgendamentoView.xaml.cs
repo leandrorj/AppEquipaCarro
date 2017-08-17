827aa2b32235514146e33189b0358553e5dde71a
Leandro I. Farias <leandro.i.farias@avanade.com>
2017-08-17 17:38:05 -0300

    Aplicao com navegação de telas: selação de carro, equipamentos e cadastro. curso1

---
+++ AppCar/AppCar/Views/AgendamentoView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppCar.Views
+{
+    public partial class AgendamentoView : ContentPage
+    {
+        public Veiculo Veiculo { get; set; }
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Email { get; set; }
+
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento
+        {
+            get
+            {
+                return dataAgendamento;
+            }
+            set
+            {
+                dataAgendamento = value;
+            }
+        }
+
+        public TimeSpan HoraAgendamento { get; set; }
+
+
+
+
+        public AgendamentoView(Veiculo veiculo)
+        {
+            InitializeComponent();
+            this.Veiculo = veiculo;
+            this.BindingContext = this;
+        }
+
+        private void buttonAgendar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento", 
+                string.Format(
+                    @"Nome: {0}
+                    Fone: {1}
+                    E-mail: {2}
+                    Data de Agendamento: {3}
+                    Hora de Agendamento: {4}",
+                    Nome, Fone, Email, DataAgendamento.ToString("dd/MM/yyy"), HoraAgendamento), "ok");
+        }
+
+    }
+}
---
+++ AppCar/AppCar/Views/ListagemView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCar.Views
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R$ {0}", Preco); }
+        }
+    }
+
+    public partial class ListagemView : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+
+        public ListagemView()
+        {
+            InitializeComponent();
+
+            this.Veiculos = new List<Veiculo>
+            {
+                new Veiculo { Nome="Azera V6", Preco = 60000},
+                new Veiculo { Nome="Fiesta", Preco = 50000},
+                new Veiculo { Nome="Vectra", Preco = 40000},
+            };
+
+            this.BindingContext = this;          
+        }
+
+        private void listViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            Navigation.PushAsync(new DetalheView(veiculo));
+
+        //    DisplayAlert("Test Drive", string.Format("você tocou no modelo '{0}', que custa {1}"
+        //       , veiculo.Nome, veiculo.PrecoFormatado), "ok");
+        }
+    }
+}
